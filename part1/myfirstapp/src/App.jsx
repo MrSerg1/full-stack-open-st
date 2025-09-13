330832e88b982cc76308c6dd355e194168b7f049
MrSerg1 <sergiotejon96@gmail.com>
2025-09-13 07:10:25 -0500

    solution part1 exercise 1.3

--- part1/myfirstapp/src/App.jsx
+++ part1/myfirstapp/src/App.jsx
@@ -1,12 +1,15 @@
 const App = () => {
 //constant definitions
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of react'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name :'Fundamentals of react:',
+    exercises : 10}
+  const part2 = {
+    name: 'Using props to pass data:',
+    exercises: 7}
+  const part3 = {
+    name:'State of a component:',
+    exercises: 14}
 //component Header
   const Header = ()=> { 
     return(
@@ -14,32 +17,23 @@ const App = () => {
     )
   }
 //component Content
-const Content = (props)=> {
-  return(
-    <>
-    <p>
-      {props.part1} {props.exercises1}
-      {props.part2} {props.exercises2}
-      {props.part3} {props.exercises3}
-    </p>
-    </>
-  )
-}
+
+
 //Component Total
-const Total = ()=> {
-  return(
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  )
-}
+//const Total = ()=> {
+//  return(
+//    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//  )
+//}
 
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1}/>
-      <Content part2={part2} exercises2={exercises2}/>
-      <Content part3={part3} exercises3={exercises3}/>
-      <Total />
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
